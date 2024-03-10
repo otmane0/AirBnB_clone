f26deb46ade299a5dca6fb87091c0134cab103de
OTMANE <davenamir3@gmail.com>
2024-03-10 23:00:24 +0100

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Base FIle"""
+"""Base File"""
 from datetime import datetime
 import uuid
 
 from models.engine.file_storage import storage
 
+
 class BaseModel:
-    """"Define Class"""
-    def __init__(self, *args, **kwargs) :
-        """"Define Class"""
+    """Define Class"""
+
+    def __init__(self, *args, **kwargs):
+        """Define Class"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now().isoformat()
         self.updated_at = datetime.now().isoformat()
@@ -27,22 +29,20 @@ class BaseModel:
             self.created_at = datetime.now().isoformat()
 
     def __str__(self):
-        """"Define Class"""
-        return (f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}")
+        """Define Class"""
+        return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}"
 
     def save(self):
-        """"Define Class"""
+        """Define Class"""
         self.updated_at = datetime.now().isoformat()
         storage.new(self)
         storage.save
 
     def to_dict(self):
-        """"Define Class"""
-
+        """Define Class"""
         new_dictionary = self.__dict__.copy()
         new_dictionary['__class__'] = self.__class__.__name__
         new_dictionary['created_at'] = self.created_at
         new_dictionary['updated_at'] = self.updated_at
 
         return new_dictionary
-
