737d28d52e4ae08f12aeccfd2c60fb8eb5a96b3c
OTMANE <davenamir3@gmail.com>
2024-03-10 23:49:41 +0100

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Base File"""
 
-from models.engine.file_storage import FileStorage
+from models import storage
 
 from datetime import datetime
 import uuid
@@ -19,28 +19,27 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
-                    pass
+                    continue
                 elif key == 'created_at':
                     self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
                 elif key == 'updated_at':
                     self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
                 else:
                     setattr(self, key, value)
-        else:
+        if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            FileStorage().new(self)
+            self.created_at = datetime.now().isoformat()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Define Class"""
-        return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """Define Class"""
         self.updated_at = datetime.now().isoformat()
-        FileStorage.new(self)
-        FileStorage.save
+        storage.new(self)
+        storage.save
 
     def to_dict(self):
         """Define Class"""
