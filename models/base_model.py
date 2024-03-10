a791a376a1f3eca5677ea778c7c7fa7d3e7fcf74
OTMANE <davenamir3@gmail.com>
2024-03-10 23:18:16 +0100

    test

--- models/base_model.py
+++ models/base_model.py
@@ -26,9 +26,11 @@ class BaseModel:
                     self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
                 else:
                     setattr(self, key, value)
-        if len(kwargs) == 0:
+        else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now().isoformat()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            FileStorage().new(self)
 
     def __str__(self):
         """Define Class"""
