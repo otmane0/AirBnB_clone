7175e86f876bfdaacef4d45aef8b4a1f662e5d0b
OTMANE <davenamir3@gmail.com>
2024-03-11 00:42:07 +0100

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Base File"""
 
-from models import storage
+import models
 
 from datetime import datetime
 import uuid
@@ -14,13 +14,13 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Define Class"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
-                elif key == 'created_at':
+                if key == 'created_at':
                     self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
                 elif key == 'updated_at':
                     self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
@@ -38,14 +38,14 @@ class BaseModel:
     def save(self):
         """Define Class"""
         self.updated_at = datetime.now().isoformat()
-        storage.new(self)
-        storage.save
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Define Class"""
         new_dictionary = self.__dict__.copy()
         new_dictionary['__class__'] = self.__class__.__name__
-        new_dictionary['created_at'] = self.created_at
-        new_dictionary['updated_at'] = self.updated_at
+        new_dictionary['created_at'] = self.created_at.isoformat()
+        new_dictionary['updated_at'] = self.updated_at.isoformat()
 
         return new_dictionary
