a0fc1ccab84dc4fe760746a819376715694cd1c3
OTMANE <davenamir3@gmail.com>
2024-03-11 02:01:53 +0100

    console test

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""console file"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = "(hbnb)"
+
+    """the console"""
+    def do_EOF(self, line):
+        """for exiting"""
+        return True
+
+    def do_quit(self, line):
+        """for exiting"""
+        return True
+
+    def do_help(self, arg: str) -> bool | None:
+        """help func"""
+        return super().do_help(arg)
+
+    def emptyline(self):
+        """empty line"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
