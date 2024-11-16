b65846f0f8df332a18f360f5e495701f92050e9e
OTMANE <davenamir3@gmail.com>
2024-11-16 23:27:12 +0100

    t to file storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,41 @@
 #!/usr/bin/python3
-"""Base File"""
+"""Basemodel class"""
 
-import models
-
-from datetime import datetime
 import uuid
-
-
+from datetime import datetime
 
 class BaseModel:
-    """Define Class"""
-
+    """Main base for all next classes"""
     def __init__(self, *args, **kwargs):
-        """Define Class"""
+        """Init"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == 'created_at':
-                    self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                elif key == 'updated_at':
-                    self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                else:
-                    setattr(self, key, value)
+
 
     def __str__(self):
-        """Define Class"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """Print class"""
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        """Define Class"""
+        """Update date_time"""
         self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
-
     def to_dict(self):
-        """Define Class"""
-        new_dictionary = self.__dict__.copy()
-        new_dictionary['__class__'] = self.__class__.__name__
-        new_dictionary['created_at'] = self.created_at.isoformat()
-        new_dictionary['updated_at'] = self.updated_at.isoformat()
+        """Return a dictionary of all instance"""
+        """my_dict = self.__dict__.copy()
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['__class__'] = self.__class__.__name__
 
-        return new_dictionary
+        return my_dict
+"""
+        my_dict = {}
+        for key, value in self.__dict__.items():
+            if isinstance(value, datetime):
+                my_dict[key] = value.isoformat()
+            else:
+                my_dict[key] = value
+
+        my_dict['__class__'] = self.__class__.__name__
+        return my_dict
+
