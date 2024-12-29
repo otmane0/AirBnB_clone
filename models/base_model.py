db664b0483dd58609a8b332b6235c6d19569d106
OTMANE <davenamir3@gmail.com>
2024-12-29 19:07:03 +0100

    s

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 
 from uuid import uuid4
 from datetime import datetime
-from models import storage
+from models.__init__ import storage
 
 class BaseModel:
     """The base class for all models in the application."""
