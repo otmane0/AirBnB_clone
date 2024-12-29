e6b5bd23e3214bca60806da6464d723db4f6c642
OTMANE <davenamir3@gmail.com>
2024-12-29 19:01:56 +0100

    dl

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Make unique Filestorage"""
 
-from models.engine.fil_storage import FileStorage
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 
