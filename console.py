d70452383ae11f95939eacc350c8dee087473270
OTMANE <davenamir3@gmail.com>
2024-12-29 22:22:45 +0100

    simple commands

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Make consol"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Console class for back-end"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, args):
+        """Exit the program"""
+        return True
+
+    def do_EOF(self, args):
+        """Exit program"""
+        print("")
+        return True
+
+    def do_emptyline(self, args):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
