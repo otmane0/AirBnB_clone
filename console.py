1617c31f456ed92d6f4fc426436ad50efe6d2169
OTMANE <davenamir3@gmail.com>
2024-12-29 22:50:15 +0100

    xx

--- console.py
+++ console.py
@@ -7,17 +7,16 @@ class HBNBCommand(cmd.Cmd):
     """Console class for back-end"""
     prompt = "(hbnb) "
 
-    def do_quit(self, args):
+    def do_quit(self, line):
         """Quit command to exit the program\n"""
         return True
 
-    def do_EOF(self, args):
-        """EOF command to exit the program\n"""
-        print()
+    def do_EOF(self, line):
+        """C-d command to exit the program\n"""
         return True
 
-    def do_emptyline(self):
-        """Do nothing on an empty line + ENTER"""
+    def emptyline(self):
+        """an empty line handling"""
         pass
 
 
