feb8f6a4834f161853bfb129906ec252d96ee802
OTMANE <davenamir3@gmail.com>
2024-12-29 18:53:34 +0100

    new_try

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new()
+            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key == "__class__":
